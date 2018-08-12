32efeb3e1dbad9e18afcbade60f0c6667e3ef337
rg872 <gilang0293@gmail.com>
2018-08-13 05:11:33 +0700

    insert little bit comment

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 require('dotenv').config()
 const translate = require('translate')
-const labels = require('./labels-eng.json')
+const labels = require('./labels-eng.json') // ini labels yang di copy dari npm sentiment
 const fs = require('fs')
 const ProgressBar = require('progress')
 
 translate.engine = 'google'
-translate.key = process.env.TRANSLATE_API_KEY
+translate.key = process.env.TRANSLATE_API_KEY // klo mau coba ntar minta gw aja key-nya
 
 const size = Object.keys(labels).length;
 const bar = new ProgressBar(':bar :percent', { total: size , width: 20});
 
+// translasi key labels satu per satu
+// lalu delete key yg bahasa inggris
+// tapi urutannya setelah di translate jadi berantakan
 async function translating(){
     try {
       for(let key in labels){
@@ -26,6 +29,9 @@ async function translating(){
     }    
 }
 
+// setelah tranlate di masukin ke dalam "labels-ind-after-tranlate.json"
+// trus isi dari "labels-ind-after-tranlate.json" gw copy ke "labels-ind-edited.json"
+// yg "labels-ind-edited.json ini yg dipakai buat edit2"
 async function begin(){
   try {
     await translating()
